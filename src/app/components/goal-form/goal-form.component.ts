63d23215b5b8e7cea955922d89e3ac9948d95911
Cencious <user.email>
2022-04-12 22:19:14 +0300

    add .

--- src/app/components/goal-form/goal-form.component.ts
+++ src/app/components/goal-form/goal-form.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Goal } from 'src/app/goal';
+Goal
 @Component({
   selector: 'app-goal-form',
   templateUrl: './goal-form.component.html',
   styleUrls: ['./goal-form.component.css']
 })
 export class GoalFormComponent implements OnInit {
-
+newGoal=new Goal()
   constructor() { }
 
   ngOnInit(): void {
